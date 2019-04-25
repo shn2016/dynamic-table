a2fafe854b0989dbc6fb22cfb6ae66a464b4abc5
Kai GAO <kaigaoinau@gmail.com>
2019-04-26 01:56:52 +1000

    Updates

---
+++ precache-manifest.7a2258f40e3d50ab312464632c84df4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b507652a0ff17c049ef7",
+    "url": "/static/css/main.e50737d0.chunk.css"
+  },
+  {
+    "revision": "b507652a0ff17c049ef7",
+    "url": "/static/js/main.760d98d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07d8b5a0e464f10458ad",
+    "url": "/static/js/2.b0c2bdad.chunk.js"
+  },
+  {
+    "revision": "317c6561857834beb29f0d903ecdc6cf",
+    "url": "/static/media/README.317c6561.md"
+  },
+  {
+    "revision": "8bd856c6f9ca044aaa3bec9af4c2ded1",
+    "url": "/index.html"
+  }
+];
